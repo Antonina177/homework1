6b3e583c941030272e745bd3fcc3de7b728ce6e9
Antonina H <justantonia17@gmail.com>
2025-09-24 17:17:07 +0200

    bbbn

---
+++ project2/my-git.js
@@ -0,0 +1,13 @@
+Цілі числа: 10, 20, 30
+Списки: [4, 5, 6], ["x", "y", "z"]
+Логічні оператори:
+(True and True) or False
+not(False or True)
+
+ //Testing QA con+ddddddd
+ 
+ Мова: Python
+
+ new one
+
+ My new branch
