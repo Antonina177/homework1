87afc9b032221563abbd0108531e0a4288bc858b
Antonina H <justantonia17@gmail.com>
2025-09-26 19:16:59 +0200

    afо

---
+++ project3/logical.js
@@ -0,0 +1,39 @@
+let a = 5;
+const b = 10;
+let c = 5;
+const d = true;
+const s = 'test';
+
+console.log(a == b);
+console.log(a == c);
+console.log(a !== b);
+console.log('===exapmle1===');
+console.log(a == b);
+console.log(a == c);
+console.log(a !== b);
+console.log('===example2===');
+console.log(a===b);
+
+a = '10test'
+c = 5;
+
+console.log('===example3===');
+console.log(a == c);
+console.log(a === c);
+
+console.log('=== example4===');
+a = 'srt1';
+c = 'str2'
+console.log(a > c);
+
+console.log('=== example5===');
+a = 'srt';
+c = 'string'
+console.log(a > c);
+console.log(a < c);
+console.log(a = c);
+
+console.log('=== example6===');
+console.log(d > c);
+c = 's';
+console.log(d > c);
