68c090d0c59cda7426f83d4cc91ee26a6e457d48
Antonina H <justantonia17@gmail.com>
2025-09-26 19:15:45 +0200

    adо

--- project3/arifmetics.js
+++ project3/arifmetics.js
@@ -16,52 +16,26 @@ console.log(c ** b);
 console.log(b % a);
 console.log(b % b);
 
-console.log(a == b);
-console.log(a == c);
-console.log(a !== b);
-console.log('===exapmle3===');
-console.log(a == b);
-console.log(a == c);
-console.log(a !== b);
-console.log('===example4===');
-console.log(a===b);
 a = '10test'
 c = 5;
-console.log('===example5===');
+console.log('===example3===');
 console.log(a + c);
-console.log(a == c);
-console.log(a === c);
-console.log('===example6===');
+console.log('===example4===');
 c *=2;
 console.log(c);
 
-console.log('=== NaN example7===');
+console.log('=== NaN example5===');
 console.log(s * c);
 
-console.log('=== example8===');
-a = 'srt1';
-c = 'str2'
-console.log(a > c);
-
-console.log('=== example9===');
-a = 'srt';
-c = 'string'
-console.log(a > c);
-console.log(a < c);
-console.log(a = c);
-
-console.log('=== example10===');
+console.log('=== example6===');
 console.log(d + c);
-console.log(d > c);
-c = 's';
-console.log(d > c);
 
-console.log('=== example11===');
+console.log('=== example7===');
 c = 10;
 c++;
 console.log(c);
 
-console.log('=== example12===');
+console.log('=== example8===');
 c = 10;
 c--;
 console.log(c);
