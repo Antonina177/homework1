aebe492901aef47e93d189ead766135777abba6a
Antonina H <justantonia17@gmail.com>
2025-09-26 20:09:30 +0200

    aggg

---
+++ project3/switch.js
@@ -0,0 +1,29 @@
+const a = 10;
+switch (a) {
+    case 1:
+        console.log(a === 1);
+        break;
+    case 2:
+        console.log(a === 2);
+        break;
+    case 3:
+        console.log(a ===3);
+        break;
+    case 4:
+        console.log(a===4);
+        break;
+    case 5:
+        console.log('Closer');
+        break;
+    case 6:
+        console.log(a===6);
+        break;
+    case 7:
+        console.log(a===7);
+        break;
+    case 10:
+        console.log ('Finally!');
+        break;
+    default:
+        console.log(a);
+}
