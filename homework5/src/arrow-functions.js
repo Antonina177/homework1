c21625fc740f688a5fb602c5e0ea09652373884d
Antonina H <justantonia17@gmail.com>
2025-10-09 17:31:49 +0200

    New arrow

--- homework5/src/arrow-functions.js
+++ homework5/src/arrow-functions.js
@@ -1,11 +1,10 @@
-function sumArray(arr) {
-    return arr
+const sumArray = (arr) =>
+    arr
         .filter(item => typeof item === 'number')
         .reduce((acc, num) => acc + num, 0);
-}
 
-const numbers = [1, 2, 3, 4, 5];
-const strings = ['1', '2', '3', '4', 'text'];
+const numbers = [10, 20, 30, 40];
+const strings = ['10', '20', '30'];
 
 console.log(sumArray(numbers));
 console.log(sumArray(strings));
