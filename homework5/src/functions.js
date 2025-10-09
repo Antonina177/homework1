9fa53a099c41e69ab4626b10eb14ba2fdcb7b97d
Antonina H <justantonia17@gmail.com>
2025-10-09 17:28:43 +0200

    new arr

---
+++ homework5/src/functions.js
@@ -0,0 +1,11 @@
+function sumArray(arr) {
+    return arr
+      .filter(item => typeof item === 'number')
+      .reduce((acc, num) => acc + num, 0);
+  }
+  
+  const numbers = [1, 2, 3, 4, 5];
+  const strings = ['1', '2', '3', '4', 'text'];
+  
+  console.log(sumArray(numbers));
+  console.log(sumArray(strings));
