ebcd5b1cb20d9656fc1fa5be7458802630352b14
Antonina H <justantonia17@gmail.com>
2025-10-09 20:19:47 +0200

    test
    :wq

--- homework7/src/arrow-functions.ts
+++ homework7/src/arrow-functions.ts
@@ -1,13 +1,14 @@
-const sumArray = (arr: (number | string | unknown)[]): number => {
+const sumArrayArrow = (arr: (number | string | unknown)[]): number => {
     return arr
       .filter((item): item is number => typeof item === 'number')
       .reduce((acc, num) => acc + num, 0);
   };
   
-  const numbers: number[] = [10, 20, 30, 40];
-  const strings: string[] = ['10', '20', '30'];
+  const numbers2: number[] = [10, 20, 30, 40];
+  const strings2: string[] = ['10', '20', '30'];
   
-  console.log(sumArray(numbers)); // 100
-  console.log(sumArray(strings)); // 0
+  console.log(sumArray(numbers));
+  console.log('-------');
+  console.log(sumArray(strings)); 
   
   
